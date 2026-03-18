aeb89621a174980bac5dfa8821cdcb4a9407cc28
ctrl-schaff <schaffjohnathan@gmail.com>
2026-03-17 22:51:40 -0500

    Simplify our hub executable

--- bin/hub.py
+++ bin/hub.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+import asyncio
 import os
-import config, biothings
+
+import biothings
+import config
+from biothings.hub import HubServer
 from biothings.utils.version import set_versions
 
 app_folder, _src = os.path.split(os.path.split(os.path.abspath(__file__))[0])
@@ -9,11 +13,13 @@ set_versions(config, app_folder)
 logging = config.logger
 
 
-from biothings.hub import HubServer
-import hub.dataload.sources
-
 if __name__ == "__main__":
-    logging.info("Hub DB backend: %s" % biothings.config.HUB_DB_BACKEND)
-    logging.info("Hub database: %s" % biothings.config.DATA_HUB_DB_DATABASE)
-    server = HubServer(hub.dataload.sources, name=config.HUB_NAME)
+    logging.info(
+        "Hub DB backend | %s\n"
+        "Hub database   | %s",
+        biothings.config.HUB_DB_BACKEND, biothings.config.DATA_HUB_DB_DATABASE
+    )
+    asyncio.set_running_loop(asyncio.new_event_loop())
+    sources = []
+    server = HubServer(sources, name=config.HUB_NAME)
     server.start()
