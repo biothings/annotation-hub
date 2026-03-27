528d3a8e3ae5f88b62bae6627cecd12d92e07ef1
ctrl-schaff <schaffjohnathan@gmail.com>
2026-03-27 11:26:27 -0500

    Update the asyncio loop construction

--- bin/hub.py
+++ bin/hub.py
@@ -2,10 +2,7 @@
 
 import asyncio
 import os
-
-import biothings
-import config
-from biothings.hub import HubServer
+import config, biothings
 from biothings.utils.version import set_versions
 
 app_folder, _src = os.path.split(os.path.split(os.path.abspath(__file__))[0])
@@ -13,13 +10,12 @@ set_versions(config, app_folder)
 logging = config.logger
 
 
+from biothings.hub import HubServer
+import hub.dataload.sources
+
 if __name__ == "__main__":
-    logging.info(
-        "Hub DB backend | %s\n"
-        "Hub database   | %s",
-        biothings.config.HUB_DB_BACKEND, biothings.config.DATA_HUB_DB_DATABASE
-    )
-    asyncio.set_running_loop(asyncio.new_event_loop())
-    sources = []
-    server = HubServer(sources, name=config.HUB_NAME)
+    logging.info("Hub DB backend: %s" % biothings.config.HUB_DB_BACKEND)
+    logging.info("Hub database: %s" % biothings.config.DATA_HUB_DB_DATABASE)
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    server = HubServer(hub.dataload.sources, name=config.HUB_NAME)
     server.start()
